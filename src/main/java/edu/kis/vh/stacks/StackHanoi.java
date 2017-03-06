2e6a2e7b89916af5b60047bc5d68bec6307c6d68
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-06 20:08:43 +0100

    Podpunkt 3.3.2

--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -1,13 +1,25 @@
 package edu.kis.vh.stacks;
 
+/**
+ * Klasa reprezentuje stos Hanoi, na ktorym nie jest mozliwe polozenie liczby wiekszej niz obecna na wierzchu stosu.
+ * 
+ * @author TworcaProgramu
+ *
+ */
 public class StackHanoi extends Stack {
 
 	private int totalRejected = 0;
 
+	/**
+	 * @return liczba odrzuconych elementow podczas prob dodania na stos
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 	
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.Stack#push(int)
+	 */
 	@Override
 	public void push(int in) {
 		if (!isEmpty() && in > top())
