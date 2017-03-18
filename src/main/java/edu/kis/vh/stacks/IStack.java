b7f50d26eb56e3ad3459bbbca52efc891a1e9a93
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-18 13:09:18 +0100

    Podpunkt 3.1.11

--- src/main/java/edu/kis/vh/stacks/IStack.java
+++ src/main/java/edu/kis/vh/stacks/IStack.java
@@ -2,7 +2,7 @@ package edu.kis.vh.stacks;
 
 public interface IStack {
 	
-	public static final int EMPTY_STACK_INDICATOR = -1;
+	public static final int EMPTY_STACK_INDICATOR = 0;
 	
 	void push(int i);
 
@@ -13,5 +13,7 @@ public interface IStack {
 	int top();
 
 	int pop();
+	
+	// 3.1.11: Realizacja zadania 3.1.10 pomogla, poniewaz w celu okreslonych zmian wystarczyla zmiana wartosci jednej stalej w intefejsie.
 
 }
