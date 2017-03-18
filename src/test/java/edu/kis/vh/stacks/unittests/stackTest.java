221216624544370c939dd3290a14345245eb064c
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-18 22:47:43 +0100

    Podpunkt 3.2.2

--- src/test/java/edu/kis/vh/stacks/unittests/stackTest.java
+++ src/test/java/edu/kis/vh/stacks/unittests/stackTest.java
@@ -7,12 +7,14 @@ import edu.kis.vh.stacks.Stack;
 import edu.kis.vh.stacks.StackHanoi;
 import edu.kis.vh.stacks.factory.ArrayStacksFactory;
 import edu.kis.vh.stacks.factory.DefaultStacksFactory;
+import edu.kis.vh.stacks.factory.ListStacksFactory;
 
 public class StackTest {
 	
 	@Test
-	public void testPush() {
-		Stack stackObj = new Stack();
+	public void testStackArrayPush() {
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		Stack stackObj = arrayStacksFactory.GetStandardStack();
 		int testValue = 4;
 		stackObj.push(testValue);
 		
@@ -21,8 +23,9 @@ public class StackTest {
 	}
 
 	@Test
-	public void testIsEmpty() {
-		Stack stackObj = new Stack();
+	public void testStackArrayIsEmpty() {
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		Stack stackObj = arrayStacksFactory.GetStandardStack();
 		boolean result = stackObj.isEmpty();		
 		Assert.assertEquals(true, result);
 		
@@ -33,7 +36,7 @@ public class StackTest {
 	}
 
 	@Test
-	public void testIsFull() {
+	public void testStackArrayIsFull() {
 		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
 		Stack stackObj = arrayStacksFactory.GetStandardStack();
 		final int STACK_CAPACITY = 12;
@@ -49,8 +52,9 @@ public class StackTest {
 	}
 
 	@Test
-	public void testTop() {
-		Stack stackObj = new Stack();
+	public void testStackArrayTop() {
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		Stack stackObj = arrayStacksFactory.GetStandardStack();
 		final int EMPTY_STACK_VALUE = -1;
 		
 		int result = stackObj.top();
@@ -66,8 +70,9 @@ public class StackTest {
 	}
 
 	@Test
-	public void testPop() {
-		Stack stackObj = new Stack();
+	public void testStackArrayPop() {
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		Stack stackObj = arrayStacksFactory.GetStandardStack();
 		final int EMPTY_STACK_VALUE = -1;
 		
 		int result = stackObj.pop();
@@ -128,4 +133,84 @@ public class StackTest {
 	
 	// 3.2.1: Niektore testy konczyly sie porazka. W celu poprawy przywrocono wartosc
 	// EMPTY_STACK_INDICATOR na -1 w interfejsie IStack (zmiana na 0 wynikala z podpunktu 11)
+	
+	@Test
+	public void testStackListPush() {
+		ListStacksFactory listStacksFactory = new ListStacksFactory();
+		Stack stackObj = listStacksFactory.GetStandardStack();
+		int testValue = 4;
+		stackObj.push(testValue);
+		
+		int result = stackObj.top();
+		Assert.assertEquals(testValue, result);
+	}
+
+	@Test
+	public void testStackListIsEmpty() {
+		ListStacksFactory listStacksFactory = new ListStacksFactory();
+		Stack stackObj = listStacksFactory.GetStandardStack();
+		boolean result = stackObj.isEmpty();		
+		Assert.assertEquals(true, result);
+		
+		stackObj.push(888);
+		
+		result = stackObj.isEmpty();
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void testStackListIsFull() {
+		ListStacksFactory listStacksFactory = new ListStacksFactory();
+		Stack stackObj = listStacksFactory.GetStandardStack();
+		final int TEST_STACKLIST_SIZE = 100;
+		
+		boolean result = stackObj.isFull();		
+		Assert.assertEquals(false, result);
+		
+		for (int i = 0; i < TEST_STACKLIST_SIZE; i++) {
+			stackObj.push(888);
+		}
+		
+		result = stackObj.isFull();
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void testStackListTop() {
+		ListStacksFactory listStacksFactory = new ListStacksFactory();
+		Stack stackObj = listStacksFactory.GetStandardStack();
+		final int EMPTY_STACK_VALUE = -1;
+		
+		int result = stackObj.top();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+		
+		int testValue = 4;
+		stackObj.push(testValue);
+		
+		result = stackObj.top();
+		Assert.assertEquals(testValue, result);
+		result = stackObj.top();
+		Assert.assertEquals(testValue, result);
+	}
+
+	@Test
+	public void testStackListPop() {
+		ListStacksFactory listStacksFactory = new ListStacksFactory();
+		Stack stackObj = listStacksFactory.GetStandardStack();
+		final int EMPTY_STACK_VALUE = -1;
+		
+		int result = stackObj.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+		
+		int testValue = 4;
+		stackObj.push(testValue);
+		
+		result = stackObj.pop();
+		Assert.assertEquals(testValue, result);
+		result = stackObj.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+	}
+	
+	// 3.2.2: Zmieniono nazwy kilku dotychczasowych testow na zwiazane z implementacja stosu w postaci StackArray
+	// Dodano testy zwiazane z implementacja stosu w postaci StackList
 }
