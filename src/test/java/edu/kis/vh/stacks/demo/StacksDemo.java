fa055fbe1d97762a850ed9bfbb6bedbbe29abd44
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-18 15:35:07 +0100

    Podpunkt 3.1.16

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -2,21 +2,27 @@ package edu.kis.vh.stacks.demo;
 
 import edu.kis.vh.stacks.StackHanoi;
 import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.factory.ArrayStacksFactory;
 import edu.kis.vh.stacks.factory.DefaultStacksFactory;
+import edu.kis.vh.stacks.factory.IStacksFactory;
+import edu.kis.vh.stacks.factory.ListStacksFactory;
 
 class StacksDemo {
 
 	public static void main(String[] args) {
 		DefaultStacksFactory factory = new DefaultStacksFactory();
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		ListStacksFactory listStacksFactory = new ListStacksFactory();
 		
 		testStacks(factory);
-		
+		testStacks(arrayStacksFactory);
+		testStacks(listStacksFactory);
 	}
 	// zle sformatowane wiersze: 19, 20, 21, 26
 	// alt + <- cofa kursor do ostatniego miejsca edycji
 	// alt + -> ponawia miejsce kursora	
 
-	private static void testStacks(DefaultStacksFactory factory) {
+	private static void testStacks(IStacksFactory factory) {
 		Stack[] stacks = { factory.GetStandardStack(), factory.GetFalseStack(),
 				factory.GetFIFOStack(), factory.GetHanoiStack()};
 		
